5d6462042543ed044d359962ecb61ab271726097
YavorPtv <yavorpetrakiev@gmail.com>
2025-07-12 23:14:54 +0300

    Add initial backend setup

--- medlink/src/components/Dashboard/Dashboard.jsx
+++ medlink/src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+
+
 export default function Dashboard(){
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/hello')
+            .then(res => res.json())
+            .then(data => setData(data.message));
+
+    }, [])
+
     return (
         <>
             <h1>This is the Dashboard!</h1>
+            <p>{data || "Loading..."} </p>
         </>
     )
 }
